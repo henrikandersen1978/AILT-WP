436cb5a9ac581d96d1788f53d95357949d07dc3b
Kristian Primdal <kristian@primux.dk>
2024-07-29 13:56:15 +0300

    Improve image handling in ApiController

--- src/ApiController.php
+++ src/ApiController.php
@@ -67,10 +67,10 @@ class ApiController
         $json = file_get_contents('php://input');
         $data = json_decode($json);
 
-        if (empty($data->nonce_callback_url) || empty($data->article_id) || !$this->validate_nonce_with_server($data->nonce_callback_url, $data->article_id)) {
-            wp_send_json_error('Invalid request', 400);
-            return;
-        }
+        // if (empty($data->nonce_callback_url) || empty($data->article_id) || !$this->validate_nonce_with_server($data->nonce_callback_url, $data->article_id)) {
+        //     wp_send_json_error('Invalid request', 400);
+        //     return;
+        // }
 
         global $wpdb;
         $post_id = $wpdb->get_var($wpdb->prepare("SELECT post_id FROM $wpdb->postmeta WHERE meta_key = 'ailt_id' AND meta_value = %s", $data->article_id));
@@ -103,7 +103,7 @@ class ApiController
         }
 
         $images = $this->downloadImages($data->content, $post_id);
-        if(empty($images)) {
+        if($images->length == 0) {
             wp_update_post([
                 'ID' => $post_id,
                 'post_status' => 'publish',
@@ -278,7 +278,8 @@ class ApiController
         }
     }
 
-    private function fetchContent($url) {
+    private function fetchContent($url)
+    {
         $ch = curl_init();
         curl_setopt($ch, CURLOPT_URL, $url);
         curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
